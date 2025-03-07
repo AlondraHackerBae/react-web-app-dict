f66c3b53c1a64d59e347ad6666332d5206dc9468
AlondraHackerBae <khrissy921@gmail.com>
2025-03-07 16:37:42 -0600

    added synonym component

--- src/Meaning.jsx
+++ src/Meaning.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -11,6 +12,7 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>{definition.definition}</p>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
